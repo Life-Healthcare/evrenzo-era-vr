f78520b27f37d64ea4f812a61885180abccff643
Enijar <TheEnijar@gmail.com>
2022-04-19 16:07:02 +0100

    chore: render scene id

--- src/pages/scene/scene.tsx
+++ src/pages/scene/scene.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Text } from "@react-three/drei";
 
 export default function Scene() {
+  const params = useParams();
+  const id = React.useMemo(() => {
+    return parseInt(params.id ?? "1");
+  }, [params]);
+
   return (
     <group>
       <mesh>
-        <boxBufferGeometry />
+        <planeBufferGeometry />
         <meshBasicMaterial color="crimson" />
       </mesh>
+      <Text position={[0, 0, 0.0001]}>{id}</Text>
     </group>
   );
 }
